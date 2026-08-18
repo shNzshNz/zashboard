b380ce92332bd3be8ad593a1022902e31bcae181
shNzshNz <shNzshNz@users.noreply.github.com>
2026-08-18 11:58:39 +0000

    deploy: 563cd9f10181209d3ffe5b51783ca72b6906b633

---
+++ assets/clash-Bi_1O0nx.js
@@ -0,0 +1,1 @@
+import{E as e,Q as t,Z as n}from"./index-07IYsVSo.js";var r=async()=>{e.value=(await n()).data},i=async e=>{await t(e),r()};export{r as fetchConfigs,i as updateConfigs};
---
+++ assets/singbox-BykRvE_S.js
@@ -0,0 +1,1 @@
+import{g as e,h as t}from"./index-07IYsVSo.js";var n=async()=>{e.value=[],t.value=[]};export{n as fetchRules};
